c78dfb7bf07357b2f1687734b90e72752ac0fe0c
Bojan SKI <codeucenje@gmail.com>
2024-04-16 15:49:51 +0200

    routes added to App.jsx. Pages created (Search, Movies and TvShows). Componenets Navbar and Footer created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import Dashboard from "./pages/Dashboard"
+import Movies from "./pages/Movies"
+import TvShows from "./pages/TvShows"
+
 const App = () => { 
   return (
-    <>
-      <h1>Hello World</h1>
-      <p>hello</p>
-    </>
+    <BrowserRouter>
+      <>
+        <Navbar/>
+
+        <main>
+          <div className="container">
+            <Routes>
+              <Route path='/' element={<Dashboard />} />
+              <Route path='movies' element={<Movies />} />
+              <Route path='tv-shows' element={<TvShows />} />
+            </Routes>
+          </div>
+        </main>
+
+        <Footer/>
+      </>
+    </BrowserRouter>
   )
 }
 
