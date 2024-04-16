8676a7037d6f5b8daad9fce1d8f5b4bce6f50ecf
Bojan SKI <codeucenje@gmail.com>
2024-04-16 16:55:53 +0200

    navbar and css for navbar and footer components done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ const Footer = () => {
     const year = new Date().getFullYear()
 
     return (
-        <footer className="footer">
-            <p>
+        <footer className="footer text-center text-white">
+            <p className="py-4 mb-0">
                 &copy; {year} BP Development. All rights reserved.
             </p>
         </footer>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,28 @@
 const Navbar = () => {
+    const active = window.location.pathname;
+
     return (
-        <header>
+        <header className="text-white mb-5">
             <div className="container d-flex align-items-center justify-content-between">
+                {/* logo */}
                 <div className="logo">
-                    <a className="navbar-brand" href="#">
+                    <a className="navbar-brand text-uppercase" href="/">
                         What To Watch App
                     </a>
                 </div>
+
+                 {/* navbar links */}
                 <nav className='navbar navbar-expand'>
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link active" href="movies">Movies</a>
+                            <a className={active === '/movies' ? "nav-link active" : "nav-link"} href="movies">
+                                Movies
+                            </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="tv-shows">Tv Shows</a>
+                            <a className={active === '/tv-shows' ? "nav-link active" : "nav-link"} href="tv-shows">
+                                Tv Shows
+                            </a>
                         </li>
                     </ul>
                 </nav>
